d330ae5db2f4d5f9d80f76e9ad30404e70d2b23f
Zolton <gmzesq@gmail.com>
2019-05-14 17:41:19 -0400

    WIP, still working on Task 2

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
 // Your code goes here
 
-console.log("hello world")
+
 const funBusImg = document.querySelector(".intro img")
-console.log(funBusImg);
 const welcome = document.querySelector(".intro h2")
+const body = document.querySelector("body")
+const firstParagraph = document.querySelector(".intro p")
+const header = document.querySelector(".main-navigation")
+// const adventureImg = document.querySelector("img/adventure.jpg")
+// const destinationImg = document.querySelector("img/destination/jpg")
+
 
 // Changes Intro h2 as you mouse over and mouseleave
 funBusImg.addEventListener("mouseover", funBusFunctionMouseOver)
@@ -15,9 +20,6 @@ function funBusFunctionMouseLeave(event) {
     welcome.textContent = "Welcome To Fun Bus!";
 }
 
-const body = document.querySelector("body")
-const firstParagraph = document.querySelector(".intro p")
-
 // Changes intro paragraph
 body.addEventListener("keydown", keyDownFunction)
 function keyDownFunction (event) {
@@ -28,13 +30,24 @@ function keyUpFunction (event) {
     firstParagraph.textContent = "Traveling expedition modern, clean webdesign blogger clean website theme website modern. Design pretty design, travelblogger adventure WordPress wanderlust theme blogger website expedition theme travelblogger. Adventure fun traveler pretty design website expedition."
 }
 
-const header = document.querySelector(".main-navigation")
-
 // Changes header when wheel scrolled
 body.addEventListener("wheel", changeColor)
 function changeColor (event) {
     header.style.backgroundColor = "lightgray"
 }
 
+// I do NOT recommend activating this one, the alert will never go away
+// body.addEventListener("drag", dragFunction)
+// function dragFunction (event) {
+//     funBusImg.src = "img/fun.jpg"
+// }
+// body.addEventListener("drop", dropFunction)
+// function dropFunction (event) {
+//     funBusImg.src = "fun-bus.jpg"
+// }
 
 
+// body.addEventListener("load", loadWelcome)
+// function loadWelcome (event) {
+//     Alert("Hello!")
+// }
